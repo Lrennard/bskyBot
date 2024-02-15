f20970459d80a7be746ffaabd0277d6247843e1c
Lrennard <lelandsrennard@gmail.com>
2024-02-14 19:57:40 -0500

    test bot script with webhook calls

--- bot.ts
+++ bot.ts
@@ -2,24 +2,38 @@ import { BskyAgent } from '@atproto/api';
 import * as dotenv from 'dotenv';
 import { CronJob } from 'cron';
 import * as process from 'process';
+import { exec } from 'child_process';
 
 dotenv.config();
 
 // Create a Bluesky Agent 
 const agent = new BskyAgent({
     service: 'https://bsky.social',
-  })
-
+});
 
 async function main() {
     await agent.login({ 
         identifier: process.env.BLUESKY_USERNAME!,
-         password: process.env.BLUESKY_PASSWORD!
-        })
+        password: process.env.BLUESKY_PASSWORD!
+    });;
+
+    const postContent = "Webhook Test";
     await agent.post({
-        text: "Hello World"
+        text: postContent
     });
-    console.log("Just posted!")
+
+    const ntfyMessage = `Just Posted:\n${postContent}`;
+    exec(`curl -L -d "${ntfyMessage}" https://ntfy.domain.example/gitpush`, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.error(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`Notification sent: ${stdout}`);
+    });
 }
 
 main();
@@ -27,8 +41,7 @@ main();
 
 // Run this on a cron job
 const scheduleExpressionMinute = '* * * * *'; // Run once every minute for testing
-const scheduleExpression = '* * * * *'; // Run once every minute in prod
+const scheduleExpression = '*/30 * * * *'; // Run once every minute in prod
 
-const job = new CronJob(scheduleExpression, main); // change to scheduleExpressionMinute for testing
-
+const job = new CronJob(scheduleExpressionMinute, main); // change to scheduleExpressionMinute for testing
 job.start();
